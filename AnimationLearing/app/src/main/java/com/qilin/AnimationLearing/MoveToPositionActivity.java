7c424d595fe9218eaa1976e6915526198fa7c69f
zql <zhangql@civaonline.cn>
2017-07-28 18:15:10 +0800

    移动动画

--- AnimationLearing/app/src/main/java/com/qilin/AnimationLearing/MoveToPositionActivity.java
+++ AnimationLearing/app/src/main/java/com/qilin/AnimationLearing/MoveToPositionActivity.java
@@ -1,12 +1,14 @@
 package com.qilin.AnimationLearing;
 
-import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
@@ -14,71 +16,84 @@ import android.widget.TextView;
  */
 
 public class MoveToPositionActivity extends AppCompatActivity {
-    private static final String TAG = "MoveToPositionActivity";
+    private final static String TAG = "MainActivity";
+    private static final int RECORD_AUDIO = 155;
+    private static final int WRITE_EXTERNAL_STORAGE = 222;
+    private TextView text;
+    TextView t1, t2;
+    RelativeLayout rlayout_root;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.movetopositionactivity);
+        setContentView(R.layout.activity_movetoposition);
+        t1 = (TextView) findViewById(R.id.txtv_1);
+        t2 = (TextView) findViewById(R.id.txtv_2);
+        rlayout_root = (RelativeLayout) findViewById(R.id.rlayout_root);
 
-        final TextView txtv_1 = (TextView) findViewById(R.id.txtv_1);
-        final TextView txtv_2 = (TextView) findViewById(R.id.txtv_2);
-        txtv_2.setOnClickListener(new View.OnClickListener() {
+
+        t1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ViewGroup.LayoutParams layoutParams = txtv_1.getLayoutParams();
-                ViewGroup.LayoutParams layoutParams2 = txtv_2.getLayoutParams();
-                final int[] txtv_1_locations = new int[2];
-                txtv_1.getLocationOnScreen(txtv_1_locations);
-                Log.i(TAG, "onClick called : x = " + txtv_1_locations[0] + " , y = " + txtv_1_locations[1]);
+                final TextView textView = new TextView(MoveToPositionActivity.this);
+                textView.setText(t1.getText());
+                textView.setTextColor(Color.YELLOW);
+                textView.setBackgroundColor(Color.BLACK);
+                textView.setGravity(Gravity.CENTER);
 
-                final int[] txtv_2_locations = new int[2];
-                txtv_2.getLocationOnScreen(txtv_2_locations);
+                int[] stc = new int[2];
+                t1.getLocationInWindow(stc);
 
-//                ObjectAnimator valueAnimator=ObjectAnimator.ofInt(txtv_2,"translationX",txtv_1_locations[0]);
+                int[] stc2 = new int[2];
+                t2.getLocationInWindow(stc2);
 
-                ValueAnimator valueX=ValueAnimator.ofInt(txtv_2_locations[0],txtv_1_locations[0]);
-                valueX.setDuration(1000);
-                valueX.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                Log.i("MainActivity", "onClick: stc =" + stc[0] + " , " + stc[1] + " , Width = "  +t1.getWidth() + " , height = " + t1.getHeight());
+                Log.i("MainActivity", "onClick: stc =" + stc2[0] + " , " + stc2[1] + " , Width = "  +t2.getWidth() + " , height = " + t2.getHeight());
+
+
+
+
+
+                RelativeLayout.LayoutParams layoutParms = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                layoutParms.setMargins(stc[0], stc[1] - (t1.getHeight() * 2), 0, 0);
+                textView.setPadding(t1.getPaddingLeft(), t1.getPaddingTop(), t1.getPaddingRight(), t1.getPaddingBottom());
+                textView.setLayoutParams(layoutParms);
+
+                rlayout_root.addView(textView);
+
+
+                int formX = 0;
+                int toX;
+                int formY = 0;
+                int toY;
+
+
+                TranslateAnimation tran = new TranslateAnimation(0, -(stc2[0] - (stc2[0] - Math.abs(t2.getWidth()))), 0, 0);
+                tran.setAnimationListener(new Animation.AnimationListener() {
                     @Override
-                    public void onAnimationUpdate(ValueAnimator animation) {
-                        int value= (int) animation.getAnimatedValue();
-                        txtv_2.setTranslationX(txtv_1_locations[0]-value);
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+//                        t2.setText(textView.getText());
+//                        textView.setVisibility(View.GONE);
+//                        rlayout_root.removeView(textView);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
                     }
                 });
-
-                ValueAnimator valueY=ValueAnimator.ofInt(txtv_2_locations[1],txtv_1_locations[1]);
-                valueX.setDuration(1000);
-                valueX.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator animation) {
-                        int value= (int) animation.getAnimatedValue();
-                        txtv_2.setTranslationY(-value);
-                    }
-                });
-
-                valueX.start();
-//                valueY.start();
+                tran.setDuration(500);
+                textView.setAnimation(tran);
+                tran.start();
 
             }
         });
 
-    }
-
-    class WrapView {
-        private View view;
-        private int moveToXY;
-
-        public WrapView(View view) {
-            this.view = view;
-        }
-
-        public int getMoveToXY() {
-            return moveToXY;
-        }
-
-        public void setMoveToXY(int moveToXY) {
-            this.moveToXY = moveToXY;
-        }
+//
     }
 }
